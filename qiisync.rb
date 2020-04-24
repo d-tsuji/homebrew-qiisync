ab3b003079d46525ce51bd1da32a739bb9fc30b7
goreleaserbot <goreleaser@carlosbecker.com>
2020-04-24 12:27:03 +0900

    Brew formula update for qiisync version v0.0.2

--- qiisync.rb
+++ qiisync.rb
@@ -2,16 +2,16 @@
 class Qiisync < Formula
   desc "Qiita CLI tool, support push and pull from/to local filesystem and Qiita."
   homepage "https://github.com/d-tsuji/qiisync"
-  version "0.0.1"
+  version "0.0.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/d-tsuji/qiisync/releases/download/v0.0.1/qiisync_0.0.1_darwin_amd64.zip"
-    sha256 "c67380e433fa3feb1596d490ba1bd995abf89a69b71d646cb4c5d5a545cae6dd"
+    url "https://github.com/d-tsuji/qiisync/releases/download/v0.0.2/qiisync_0.0.2_darwin_amd64.zip"
+    sha256 "569b76654b5d7e24cee2951ab7ba72dc046411f79b7cb578ec1d96024d9064fa"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/d-tsuji/qiisync/releases/download/v0.0.1/qiisync_0.0.1_linux_amd64.zip"
-      sha256 "80a44fff0a19f9d1547ccd1f4a4799f6bcf9617d44caadb7bf45c11c3f07fcd0"
+      url "https://github.com/d-tsuji/qiisync/releases/download/v0.0.2/qiisync_0.0.2_linux_amd64.zip"
+      sha256 "b7ff8bf0960ba4256fe593e1a4f4bcf48716957e53f8296bea329e4b4825eb12"
     end
   end
 
